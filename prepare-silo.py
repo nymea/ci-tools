95e1ea6dc8177599e31ac0d1a3832bbfa3f5698b
Jenkins <jenkins@nymea.io>
2019-07-03 16:50:26 +0200

    update prepare-silo.py to deal with gitlab too

--- prepare-silo.py
+++ prepare-silo.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import simplejson
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 import subprocess
 from time import gmtime, strftime
 
@@ -17,58 +17,104 @@ def dbg(*args, **kwargs):
     print(*args, **kwargs)
     sys.stdout.flush()
 
+def parse_repo_url(url):
+  repo_user = repository.split(':')[1].split('.')[0].split('/')[0]
+  repo_name = repository.split(':')[1].split('.')[0].split('/')[1]
+  return repo_user, repo_name
+
+def get_pull_requests(repository, tag_name, token):
+  repo_user, repo_name = parse_repo_url(repository)
+  if "github" in repository:
+    return get_pull_requests_github(repo_user, repo_name, tag_name, token)
+  elif "gitlab.nymea.io" in repository:
+    return get_pull_requests_gitlab(repo_user, repo_name, tag_name, token)
+
+def get_pull_requests_github(repo_user, repo_name, tag_name, token):
+  request = Request("http://api.github.com/repos/%s/%s/pulls" % (repo_user, repo_name))
+  request.add_header('Authorization', "token %s" % token)
+  pull_requests_data = simplejson.load(urlopen(request))
+  pull_requests = []
+  users_cache = {}
+  for pull_request_data in pull_requests_data:
+    for label in pull_request_data["labels"]:
+      if label["name"] == tag_name:
+        pull_request = {}
+        pull_request["branch_name"] = pull_request_data["head"]["ref"]
+        pull_request["title"] = pull_request_data["title"]
+        pull_request["number"] = pull_request_data["number"]
+        comitter_user = pull_request_data["user"]["login"]
+        if comitter_user not in users_cache:
+          user_data = simplejson.load(urlopen(pull_request_data["user"]["url"]))
+          comitter_name = user_data["name"]
+          users_cache[comitter_user] = comitter_name
+        pull_request["author"] = users_cache[comitter_user]
+        pull_requests.insert(0, pull_request)
+        break
+  return pull_requests
+
+def get_pull_requests_gitlab(repo_user, repo_name, tag_name, token):
+  projects_data = simplejson.load(urlopen("https://gitlab.nymea.io/api/v4/projects?private_token=%s&per_page=1000" % token))
+  project_id = -1;
+  for project_data in projects_data:
+    if project_data["path_with_namespace"] == "%s/%s" % (repo_user, repo_name):
+      project_id = project_data["id"]
+      break
+  if project_id == -1:
+    print("E: Could not find repository %s/%s" % (repo_user, repo_name))
+    exit(1)
+  
+  pull_requests = []
+  merges_data = simplejson.load(urlopen("https://gitlab.nymea.io/api/v4/projects/%s/merge_requests?private_token=%s&labels=%s" % (project_id, token, tag_name)))
+  for merge_data in merges_data:
+    pull_request = {}
+    pull_request["branch_name"] = merge_data["source_branch"]
+    pull_request["title"] = merge_data["title"]
+    pull_request["number"] = merge_data["iid"]
+    pull_request["author"] = merge_data["author"]["name"]
+    pull_requests.insert(0, pull_request)
+  return pull_requests
+
 args = sys.argv
-if len(args) < 3:
-  dbg("usage: %s <tag> <repo1> ..." % args[0])
+if len(args) < 2:
+  dbg("usage: %s <repo> <tag> [token]" % args[0])
   exit(1)
 
-silo_name = "%s-silo" % args[1]
-tag_name = args[1]
-repository = args[2]
+repository = args[1]
+silo_name = "%s-silo" % args[2]
+tag_name = args[2]
+token = ""
+if len(args) > 3:
+  token = args[3]
 
-dbg("I: Preparing silo \"%s\" using tag \"%s\"" % (silo_name, tag_name))
+dbg("I: Preparing silo \"%s\" using tag \"%s\" for repository \"%s\"" % (silo_name, tag_name, repository))
+repo_user, repo_name = parse_repo_url(repository)
 
-dbg("I: Working on repository: %s" % repository)
-repo_user = repository.split(':')[1].split('.')[0].split('/')[0]
-repo_name = repository.split(':')[1].split('.')[0].split('/')[1]
-dbg("I: Preparing silo for repository: %s (%s/%s)" % (repository, repo_user, repo_name))
-pull_requests_data = simplejson.load(urlopen("http://api.github.com/repos/%s/%s/pulls" % (repo_user, repo_name)))
-pull_requests = []
-for pull_request_data in pull_requests_data:
-  for label in pull_request_data["labels"]:
-    if label["name"] == tag_name:
-      pull_requests.insert(0, pull_request_data)
-      break
+pull_requests = get_pull_requests(repository, tag_name, token)
 
 if len(pull_requests) == 0:
   dbg("W: No pull requests with tag \"%s\" found for %s." % (tag_name, repository))
   exit(2)
 
+call("rm", "-rf", "%s" % repo_name)
 call("git", "clone", repository, repo_name)
 os.chdir(repo_name)
 call("git", "checkout", "-b", silo_name)
 
 for pull_request in pull_requests:
-  branch_name = pull_request["head"]["ref"]
-  pr_title = pull_request["title"]
   pr_number = pull_request["number"]
+  pr_title = pull_request["title"]
+  branch_name = pull_request["branch_name"]
   commit_msg = "Merge PR #%i: %s" % (pr_number, pr_title)
   dbg("I: %s (branch: %s)" % (commit_msg, branch_name))
   call("git", "merge", "--no-ff", ("origin/%s" % branch_name), "-m", commit_msg)
 
 #  call("dch", "-i", "-U", "Prepare for release")
 
-users_cache = {}
 for pull_request in pull_requests:
-  comitter_user = pull_request["user"]["login"]
-  if comitter_user not in users_cache:
-    user_data = simplejson.load(urlopen(pull_request["user"]["url"]))
-    comitter_name = user_data["name"]
-    users_cache[comitter_user] = comitter_name
-
+  author = pull_request["author"]
   pr_title = pull_request["title"]
   env_override = os.environ.copy()
-  env_override["DEBFULLNAME"] = users_cache[comitter_user]
+  env_override["DEBFULLNAME"] = author
   call("dch", "-U", pr_title, env=env_override)
 
 # Finalize release
@@ -82,9 +128,9 @@ if merge_result == 0:
   print("I: No changes since last build. Nothing to do...")
   os.chdir("..")
   exit(3);
-else
+else:
   print("There are changes. Rebuild required")
   call("git", "push", "origin", silo_name, "-f")
 
 os.chdir("..")
-exit 0
+exit(0)
